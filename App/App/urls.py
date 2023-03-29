1e14ef0411fbb85af45aa058a5a13b2c71ba9653
Rudy <bourez.rudy@gmail.com>
2023-03-29 17:01:13 +0200

    listin

--- App/App/urls.py
+++ App/App/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import home
+from main.views import listing
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home)
+    path('listing/', listing, name='listing')
 ]
--- App/main/views.py
+++ App/main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import json
 
-def home(request):
-    return render(request, 'home.html')
+def listing(request):
+    df = pd.DataFrame(np.array([["0000000", "A", 3], ["0000001", "B", 6], ["0000002", "C", 9]]),
+                   columns=['Matricule', 'Libellé', 'Nb_virements'])
+    json_records = df.to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+    return render(request, 'listing.html', context)
